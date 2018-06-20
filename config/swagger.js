e9fd30e21b6a64f34a9ca138c3808074fae4014e
gibranparvez <32872347+gibranparvez@users.noreply.github.com>
2018-06-20 07:47:41 -0400

    Swagger Doc Authentication (#22)
    * Add Authorization header auth
    
    * Rename JWT

--- config/swagger.js
+++ config/swagger.js
@@ -4,7 +4,18 @@ const options = {
         'description': 'Swagger documentation for Pelias API',
         'title': 'Pelias API',
         'version': '1.0.0'
-    }
+    },
+    'schemes': ['http','https'],
+    'securityDefinitions': { 
+        'JWT': {
+            'type': 'apiKey',
+            'name': 'Authorization',
+            'in': 'header'
+        }
+    },
+    'security': [
+       { 'JWT': []} 
+    ]
     },
     'basePath': __dirname,
     'apis': ['./routes/*.js']
